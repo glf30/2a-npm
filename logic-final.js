73768a16a2430070de0694c316c8fd443608b0bd
glf30 <greyson.frazier@gmail.com>
2025-07-08 16:31:47 -0400

    Update logic-final.js

--- logic-final.js
+++ logic-final.js
@@ -1,13 +1,25 @@
-const {
-  Celcius,
-  Fahrenheit,
-} = require("@khanisak/temperature-converter");
+// 0. Import the Fahrenheit and Celcius classes from the third-party library
+const { Fahrenheit, Celcius } = require("@khanisak/temperature-converter")
 
 // 1. Write a function that takes degrees fahrenheit and returns the temperature in degrees celsius.
-// Now utilizing the Fahrenheit class from @khanisak/temperature-converter
-function fahrenheitToCelsius(degrees) {
+function fahrenheitToCelcius(degrees) {
+  // call the function the same way as the documentation has
   return new Fahrenheit(degrees).toCelcius().value;
+  // .toCelcius gives us an object like this:
+  /*
+  Celcius {
+  value: 0,
+  toCelcius: [Function (anonymous)],
+  toDelisle: [Function (anonymous)],
+  toFahrenheit: [Function (anonymous)],
+  unit: { name: 'Celcius', code: '°C' }     
 }
+  */
+ // we only want THE VALUE!! 
+//   const celcius = new Fahrenheit(degrees).toCelcius();
+//   return celcius;
+}
+
 // You can convert degrees fahrenheit to degrees celsius by:
 // 1. subtracting 32 from the degrees fahrenheit
 // 2. multiplying the result by 5
@@ -16,13 +28,15 @@ function fahrenheitToCelsius(degrees) {
 /**
  * fahrenheitToCelsius
  * -------------------
- * Converts degrees fahrenheit to celsius using Fahrenheit class from the @khanisak/temperature-converter library.
+ * Converts degrees fahrenheit to celsius.
  * @param {number} degrees - The temperature in degrees fahrenheit.
  * @returns {number} - The temperature in degrees celsius.
  */
 
 // 3. (Don't forget to do 2 at the bottom of the file first, and test it!) Write a function that takes degrees celsius and returns the temperature in degrees fahrenheit.
-// Now utilizing the Celcius class from @khanisak/temperature-converter
+const celciusToFahrenheit = (degrees) => {
+	return new Celcius(degrees).toFahrenheit().value;
+}
 
 // You can convert degrees celsius to degrees fahrenheit by:
 // 1. multiplying the degrees celsius by 9
@@ -32,34 +46,37 @@ function fahrenheitToCelsius(degrees) {
 /**
  * celsiusToFahrenheit
  * -------------------
- * Converts degrees celsius to fahrenheit using Celcius class from the @khanisak/temperature-converter library.
+ * Converts degrees celsius to fahrenheit.
  * @param {number} degrees - The temperature in degrees celsius.
  * @returns {number} - The temperature in degrees fahrenheit.
  */
-function celsiusToFahrenheit(degrees) {
-  return new Celcius(degrees).toFahrenheit().value;
-}
 
 // 5. (Don't forget to do 4 at the bottom of the file first and test it!) Write a function that takes a temperature and a scale (either "f" for fahrenheit or "c" for celsius) and returns the temperature in the other scale. As a bonus, use the functions you wrote above to do this.
 
 /**
  * convertTemperature
  * -------------------
- * Converts degrees in the given scale to the other scale using wrapper functions above.
+ * Converts degrees in the given scale to the other scale.
  * @param {number} degrees - The temperature.
  * @param {string} scale - The scale to convert from.
  * @returns {number} - The temperature in the other scale.
  */
-function convertTemperature(degrees, scale) {
-  if (scale.toLowerCase() === "f" && typeof scale === "string") {
-    return fahrenheitToCelsius(degrees);
-  } else if (scale.toLowerCase() === "c" && typeof scale === "string") {
-    return celsiusToFahrenheit(degrees);
-  } else {
-    throw new Error("should return null if the scale is not 'f' or 'c'");
-  }
-}
 
+function convertTemperature(degrees, scale) {
+	// check our scale - "f" or "F" or "c" or "C"
+	// if "f" -> convert to celcius
+	if (scale.toLowerCase() === "f"){
+		return fahrenheitToCelcius(degrees)
+	} else if (scale.toLowerCase() === "c"){
+		return celciusToFahrenheit(degrees)
+	} else {
+		// custom error that we create and specify the message
+		//throw new Error(`User should enter "f" or "c"`)
+		console.log((`User should enter "f" or "c"`))
+		return null;
+	}
+
+}
 // 2. Export the fahrenheitToCelsius function in an object so that it can be imported in other files. Then run the tests to make sure your function works.
 
 // 4. Export the celsiusToFahrenheit function in the object at the bottom of the file. Then run the tests to make sure your function works.
@@ -67,7 +84,7 @@ function convertTemperature(degrees, scale) {
 // 6. Export the convertTemperature function in the object at the bottom of the file. Then run the tests to make sure your function works.
 
 module.exports = {
-  fahrenheitToCelsius,
-  celsiusToFahrenheit,
-  convertTemperature,
+	fahrenheitToCelcius,
+	celciusToFahrenheit,
+	convertTemperature
 };
